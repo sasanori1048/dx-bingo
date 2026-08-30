a92783693b68771ca4978c090f77626535e79e0b
sasanori1048 <sasanori1048@gmail.com>
2026-08-30 11:08:07 +0900

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -266,4 +266,20 @@ document.getElementById("modalOverlay").addEventListener("click", function(e) {
 /* ------------------------------------------------------------
    ページ読み込み時に初期化
 ------------------------------------------------------------ */
-init();
+init();  // ← まずマスを作る
+
+// ★そのあとで URLパラメータ処理を実行する
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+if (id) {
+    const targetCell = document.querySelector(`.bingo-cell[data-id='${id}']`);
+    if (targetCell) {
+        const index = Number(id) - 1;
+        stamped.add(index);
+        targetCell.classList.add("stamped");
+        checkBingo();
+        updateUI();
+    }
+}
+
