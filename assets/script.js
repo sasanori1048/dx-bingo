6ca689f9031cc0370cc87b32b8cd45077a2c513c
sasanori1048 <sasanori1048@gmail.com>
2026-08-30 11:35:02 +0900

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -95,8 +95,8 @@ if (id) {
         // ★内部状態も更新する（これが重要）
         const index = Number(id) - 1;
         stamped.add(index);
-　　　　// ★保存する
-       localStorage.setItem("stamped", JSON.stringify([...stamped]));
+　　　　 // ★保存する
+        localStorage.setItem("stamped", JSON.stringify([...stamped]));
 
         // 見た目も更新
         targetCell.classList.add("stamped");
